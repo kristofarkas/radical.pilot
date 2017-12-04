90e3ded0b9c93704c228c4fd73d1769c861a8d6c
Andre Merzky <andre@merzky.net>
2017-12-04 01:57:21 +0100

    typo

--- src/radical/pilot/agent/executing/popen.py
+++ src/radical/pilot/agent/executing/popen.py
@@ -422,7 +422,7 @@ prof(){
 
                     # We got a request to cancel this cu
                     action += 1
-                    self._log.debug('Killing process ',cu['proc'].pid)
+                    self._log.info('Killing process %s',cu['proc'].pid)
                     cu['proc'].kill()
                     cu['proc'].wait() # make sure proc is collected
 
